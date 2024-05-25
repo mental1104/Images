b901723a23f2dcd5521c84ac2bae7ae27b9431f7
mental1104 <mental1104@gmail.com>
2024-05-25 15:07:52 +0800

    postgres服务化

--- rmats2sashimiplot/src/MISO/misopy/sashimi_plot/plot.py
+++
@@ -1,9 +0,0 @@
-##
-## Old Sashimi plot interface
-##
-def main():
-    print "plot.py interface to Sashimi plot is now deprecated. " \
-          "Please run \'sashimi_plot\' instead."
-
-if __name__ == "__main__":
-    main()
